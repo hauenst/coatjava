ef38655ea4aeedb387c34246d853e6e5365d405b
Florian Hauenstein <hauenst@jlab.org>
2026-04-07 22:34:51 -0400

    Add geometry factories for LDRD urwell test stand

---
+++ common-tools/clas-jcsg/src/main/java/org/jlab/detector/geant4/v2/LMU/LMUConstants.java
@@ -0,0 +1,129 @@
+package org.jlab.detector.geant4.v2.LMU;
+
+
+import org.jlab.detector.calib.utils.DatabaseConstantProvider; 
+
+
+public class LMUConstants {
+
+    private final static String CCDBPATH = "/geometry/urwell/";
+    
+    public final static int NREGIONS    = 6;    //number of regions (1 & 6 are GEM, 2-5 are URWELL) 
+    public final static int NLAYERS     = 2;    //number of layers per region (SHOULD BE AN ARRAY)
+
+    public final static double XSIZE = 10;
+    public final static double YSIZE = 10;
+    
+    public final static double XENLARGEMENT = 0.1; // cm
+    public final static double YENLARGEMENT = 0.1;  // cm
+    public final static double ZENLARGEMENT = 0.1; // cm
+
+    public static double[] CHAMBERVOLUMESTHICKNESS;
+    public static String[] CHAMBERVOLUMESNAME;
+          
+        public final static double[] GEM_CHAMBERVOLUMESTHICKNESS = {0.0025, 0.0005,0.3,                       // window
+                                                                0.005, 0.0005,0.3,                            // cathode
+                                                                0.005, 0.05, 0.005,                           // GEM layer 1
+                                                                0.005, 0.05, 0.005,                           // GEM layer 2
+                                                                0.005, 0.05, 0.005, 0.0028,                   // GEM layer 3
+                                                                0.0037,  0.1, 0.0037,                         // Readout1
+                                                                0.0125, 0.1, 0.0037};                         // Readout2
+
+                                                            
+       
+        public final static double[] URWELL_CHAMBERVOLUMESTHICKNESS = {0.0025, 0.0005,0.3,                   // window
+                                                               0.005, 0.0005,0.3,                            // cathode
+                                                               0.0001, 0.005,                                // uRWell
+                                                               0.0005, 0.0028,                               // DlC
+                                                               0.0009, 0.0028,                               // Capacitive sharing layer1
+                                                               0.0029, 0.0075,                               // Capacitive sharing layer2
+                                                               0.0037,  0.1, 0.0037,                         // Readout1
+                                                               0.0125, 0.1, 0.0037};                         // Readout2
+
+        public final static String[] GEM_CHAMBERVOLUMESNAME = {"window_kapton", "window_Al", "window_gas",
+                                                               "cathode_kapton", "cathode_Cu", "cathode_gas",
+                                                               "gem1_Cu", "gem1_kapton", "gem1_Cu",
+                                                               "gem2_Cu", "gem2_kapton", "gem2_Cu",
+                                                               "gem3_Cu", "gem3_kapton", "gem3_Cu", "gem3_glue",
+                                                               "readout1_Cu", "readout1_g10", "readout1_Cu", 
+                                                               "readout2_glue", "readout2_g10", "readout2_Cu"};                                                       
+
+        public final static String[] URWELL_CHAMBERVOLUMESNAME = {"window_kapton", "window_Al", "window_gas",
+                                                          "cathode_kapton", "cathode_Cu", "cathode_gas",
+                                                          "muRwell_Cu", "muRwell_kapton",
+                                                          "dlc_dlc", "dlc_glue",
+                                                          "capa_sharing_layer1_Cu","capa_sharing_layer1_glue",
+                                                          "capa_sharing_layer2_Cu","capa_sharing_layer2_glue",
+                                                          "readout1_Cu", "readout1_g10", "readout1_Cu", 
+                                                          "readout2_glue", "readout2_g10", "readout2_Cu"};        
+
+    // URWELL position in the CLAS12 frame 
+    public final static double DIST2TGT[] = new double[NREGIONS];
+    
+  //  public final static double DIST2TGT   = (TGT2DC0-URWELL2DC0);
+   // public final static double W2TGT = DIST2TGT/Math.cos(Math.toRadians(THTILT-THMIN));
+  //  public final static double YMIN = W2TGT*Math.sin(Math.toRadians(THMIN)); // distance from the base chamber1 and beamline
+  //  public final static double ZMIN = W2TGT*Math.cos(Math.toRadians(THMIN));   
+    public final static double PITCH = 0.1 ;       // cm
+    public final static double[] STEREOANGLE = {0, 90, 45};   // deg
+    
+    static {
+      for (int region = 1; region <= 6; region++) {
+          initializeChamberArraysForRegion(region);
+      }
+  }
+
+  public static void initializeChamberArraysForRegion(int region) {
+      if (region == 1 || region == 6) { // GEM detectors
+          CHAMBERVOLUMESTHICKNESS = GEM_CHAMBERVOLUMESTHICKNESS;
+          CHAMBERVOLUMESNAME = GEM_CHAMBERVOLUMESNAME;
+      } else { // URWELL detectors
+          CHAMBERVOLUMESTHICKNESS = URWELL_CHAMBERVOLUMESTHICKNESS;
+          CHAMBERVOLUMESNAME = URWELL_CHAMBERVOLUMESNAME;
+      }
+  }
+
+    
+    /*
+     * @return String a path to a directory in CCDB of the format {@code "/geometry/detector/"}
+     */
+    public static String getCcdbPath()
+    {
+            return CCDBPATH;
+    }
+
+
+
+     /**
+     * Loads the the necessary tables for the URWELL geometry for a given DatabaseConstantProvider.
+     * 
+     * @return DatabaseConstantProvider the same thing
+     */
+    public static DatabaseConstantProvider connect( DatabaseConstantProvider cp )
+    {
+          //  cp.loadTable( CCDBPATH +"RWELL");
+
+            load(cp  );
+            return cp;
+    }
+
+    /**
+     * Reads all the necessary constants from CCDB into static variables.
+     * Please use a DatabaseConstantProvider to access CCDB and load the following tables:
+     * @param cp a ConstantProvider that has loaded the necessary tables
+     */
+    
+    public static synchronized void load( DatabaseConstantProvider cp )
+    {
+            // read constants from svt table
+//            NREGIONS = cp.getInteger( CCDBPATH+"svt/nRegions", 0 );
+
+             for (int i=0; i<NREGIONS; i++){
+                
+                DIST2TGT[i] = 10 + 9*i;  
+  
+            }  
+    }
+   }
+
+  
---
+++ common-tools/clas-jcsg/src/main/java/org/jlab/detector/geant4/v2/LMU/LMUGeant4Factory.java
@@ -0,0 +1,348 @@
+package org.jlab.detector.geant4.v2.LMU;
+
+import eu.mihosoft.vrl.v3d.Vector3d;
+import org.jlab.detector.geant4.v2.Geant4Factory;
+import org.jlab.detector.volume.G4World;
+import org.jlab.detector.volume.Geant4Basic;
+import org.jlab.detector.calib.utils.DatabaseConstantProvider;
+import org.jlab.detector.volume.G4Box;
+
+
+/**
+ * Generate GEANT4 volume for the URWELL detector
+ * 
+ * @author bondi
+ */
+public final class LMUGeant4Factory extends Geant4Factory {
+    
+    private int nRegions  = LMUConstants.NREGIONS;
+    
+    
+
+    /**
+     * Create the URWELL full geometry
+     * @param cp
+     */
+    public LMUGeant4Factory( DatabaseConstantProvider cp) {
+        LMUConstants.connect(cp );
+        this.init(cp);
+    }
+    
+    public void init(DatabaseConstantProvider cp) {
+   
+        motherVolume = new G4World("root");
+        nRegions = LMUConstants.NREGIONS;
+        
+
+        for (int iregion = 0; iregion <nRegions ; iregion++) {
+            Geant4Basic sectorVolume = createRegion(iregion);
+            sectorVolume.setMother(motherVolume);
+        }
+    }
+
+    /**
+     * Calculates the total detector thickness from the sum of the individual
+     * layers thicknesses
+     *   
+     * @return thickness in cm
+     */
+    // public double getChamberThickness(){
+    //     double chamberT =0;
+    //      for (int i=0; i< LMUConstants.CHAMBERVOLUMESTHICKNESS.length; i++ )
+    //          chamberT+=LMUConstants.CHAMBERVOLUMESTHICKNESS[i];
+    //      return chamberT;
+    // }
+
+   ////////sara
+//    public double getChamberThickness() {
+//     double chamberT = 0;
+
+//     for (int region = 1; region <= LMUConstants.NREGIONS; region++) {
+//         // Select the appropriate thickness array based on region
+//         double Thickness = (region == 1 || region == 6) ? 
+//                                   LMUConstants.GEM_CHAMBERVOLUMESTHICKNESS : 
+//                                   LMUConstants.URWELL_CHAMBERVOLUMESTHICKNESS;
+        
+//         // Sum up the thickness for this region
+//         //for (double thickness : thicknessArray) {
+//             chamberT += thickness;
+//             System.out.println("Region " + region + "Total Thickness: " + chamberT + " cm");
+
+//         //}
+//     }
+    
+//             return chamberT;
+//     }
+    ///////
+    public double getChamberThickness() {
+        double chamberT = 0;
+    
+        // Loop through regions 1 to 6
+        for (int region = 1; region <= 6; region++) {
+            // Check for GEM or URWELL based on the region
+            if (region == 1 || region == 6) {
+                // Use GEM_CHAMBERVOLUMESTHICKNESS
+                for (int i = 0; i < LMUConstants.GEM_CHAMBERVOLUMESTHICKNESS.length; i++) {
+                    chamberT += LMUConstants.GEM_CHAMBERVOLUMESTHICKNESS[i];
+                    System.out.println("Region " + region + " - Adding GEM thickness: " + LMUConstants.GEM_CHAMBERVOLUMESTHICKNESS[i] + " cm");
+                }
+            } 
+            else {
+                // Use URWELL_CHAMBERVOLUMESTHICKNESS for regions 2, 3, 4, 5
+                for (int i = 0; i < LMUConstants.URWELL_CHAMBERVOLUMESTHICKNESS.length; i++) {
+                    chamberT += LMUConstants.URWELL_CHAMBERVOLUMESTHICKNESS[i];
+                    System.out.println("Region " + region + " - Adding URWELL thickness: " + LMUConstants.URWELL_CHAMBERVOLUMESTHICKNESS[i] + " cm");
+                }
+            }
+            System.out.println("Region " + region + "Total Thickness: " + chamberT + " cm");
+        }
+                
+
+        return chamberT;
+    }
+
+    
+    /**
+     * Calculates the sector dimensions
+     * @p    * @return an array of doubles containing trapezoid dimensions:     
+     **  half thickness, half small base , half large base, half height, tilt angle
+     */
+    
+    // public double[] getRegionDimensions(){
+    //     double[] regionDimension = new double[5];
+
+    public double[] getRegionDimensions(int region) {
+        double[] regionDimension = new double[3];    
+        regionDimension[0] = LMUConstants.XSIZE/2 + LMUConstants.XENLARGEMENT ;
+        regionDimension[1] = LMUConstants.YSIZE/2 + LMUConstants.YENLARGEMENT ;
+        regionDimension[2] = (this.getChamberThickness())/2. + LMUConstants.ZENLARGEMENT ;
+               
+        return regionDimension;
+    }
+    
+        // Baricenter coordinate in CLAS12 frame
+    
+    /**
+    * Calculates sector baricenter coordinate in CLAS12 frame
+     * @param iregion
+     * @return Vector3d (X,Y,Z)
+    */
+    
+    public Vector3d getRegionPosition(int iregion)
+    {
+        
+        Vector3d vCenter = new Vector3d(0, 0, LMUConstants.DIST2TGT[iregion]);
+        
+        return vCenter;
+    }
+    
+
+    /**
+     * Creates and positions the region volume in the given sector, and 
+        populates it with the three chamber volumes
+     * @param iregion (0)
+     * @return the region volume
+     */
+    // public Geant4Basic createRegion(int iregion) {
+
+    //     double[] dim = this.getRegionDimensions();
+    
+    //     double regionDX   = dim[0] ;
+    //     double regionDY   = dim[1] ;
+    //     double regionDZ   = dim[2] ;
+        
+        
+    //     Vector3d vCenter = this.getRegionPosition(iregion);
+
+    //     Geant4Basic regionVolume = new G4Box("LMU_region" + (iregion + 1), regionDX, regionDY, regionDZ);
+    //     regionVolume.translate(vCenter.x, vCenter.y, vCenter.z);
+    //     regionVolume.setId(iregion + 1, 0, 0);
+        
+    //     Geant4Basic chamberVolume = this.createChamber(iregion);
+
+    //     chamberVolume.setName("region" + (iregion + 1));
+           
+    //     chamberVolume.setMother(regionVolume);
+    //     chamberVolume.setId(iregion + 1, 1, 0);
+
+               
+    //     return regionVolume;
+    // }
+
+    public Geant4Basic createRegion(int iregion) {
+        int regionIndex = iregion + 1;
+        double[] dim = this.getRegionDimensions(regionIndex);
+
+        double regionDX = dim[0];
+        double regionDY = dim[1];
+        double regionDZ = dim[2];
+
+        Vector3d vCenter = this.getRegionPosition(iregion);
+
+        Geant4Basic regionVolume = new G4Box("LMU_region" + regionIndex, regionDX, regionDY, regionDZ);
+        regionVolume.translate(vCenter.x, vCenter.y, vCenter.z);
+        regionVolume.setId(regionIndex, 0, 0);
+
+        Geant4Basic chamberVolume = this.createChamber(iregion);
+        chamberVolume.setName("region" + regionIndex);
+        chamberVolume.setMother(regionVolume);
+        chamberVolume.setId(regionIndex, 1, 0);
+
+        return regionVolume;
+    }
+
+    
+
+    /**
+     * Creates the chamber volume 
+     * 
+     * @param iRegion (0)
+     * @return the chamber volume
+     */  
+    // public Geant4Basic createChamber(int iRegion) {
+                
+    //     double chamberDX    = this.getChamberDimensions()[0];     
+    //     double chamberDY    = this.getChamberDimensions()[1];
+    //     double chamberDZ    = this.getChamberDimensions()[2];        
+
+    //     Geant4Basic chamberVolume = new G4Box("region" + (iRegion + 1), chamberDX, chamberDY, chamberDZ);
+
+    //     double daughterDX  = this.getDaughterChamberDimensions()[0];
+    //     double daughterDY  = this.getDaughterChamberDimensions()[1];
+        
+    //     double  daughterVolumeZ =0;
+       
+    //     for (int i=0; i< LMUConstants.CHAMBERVOLUMESTHICKNESS.length; i++ ){
+ 
+    //         if(i==0) {
+    //             daughterVolumeZ = LMUConstants.CHAMBERVOLUMESTHICKNESS[i]/2 - (this.getChamberThickness())/2.;
+    //          } 
+    //         else {
+    //             daughterVolumeZ += LMUConstants.CHAMBERVOLUMESTHICKNESS[i-1]/2 + LMUConstants.CHAMBERVOLUMESTHICKNESS[i]/2;
+    //         }
+            
+    //         Geant4Basic daughterVolume = new G4Box("daughter_volume", daughterDX, daughterDY,
+    //             LMUConstants.CHAMBERVOLUMESTHICKNESS[i]/2);
+            
+    //         daughterVolume.setName("region" + (iRegion + 1) +"_"+LMUConstants.CHAMBERVOLUMESNAME[i] );
+            
+            
+    //         daughterVolume.setMother(chamberVolume);
+    //         daughterVolume.setPosition(0.0, 0.0,daughterVolumeZ);
+    //     }
+    //     return  chamberVolume;
+    // }
+
+    public Geant4Basic createChamber(int iRegion) {
+        int regionIndex = iRegion + 1;
+        double chamberDX = LMUConstants.XSIZE / 2 + LMUConstants.XENLARGEMENT;
+        double chamberDY = LMUConstants.YSIZE / 2 + LMUConstants.YENLARGEMENT;
+        double chamberDZ = this.getChamberThickness() / 2.0;
+
+        Geant4Basic chamberVolume = new G4Box("region" + regionIndex, chamberDX, chamberDY, chamberDZ);
+
+        double[] thicknessArray = (regionIndex == 1 || regionIndex == 6) ? 
+                                   LMUConstants.GEM_CHAMBERVOLUMESTHICKNESS : 
+                                   LMUConstants.URWELL_CHAMBERVOLUMESTHICKNESS;
+
+        String[] nameArray = (regionIndex == 1 || regionIndex == 6) ? 
+                              LMUConstants.GEM_CHAMBERVOLUMESNAME : 
+                              LMUConstants.URWELL_CHAMBERVOLUMESNAME;
+
+        double daughterVolumeZ = -chamberDZ;
+        for (int i = 0; i < thicknessArray.length; i++) {
+            double daughterThickness = thicknessArray[i] / 2.0;
+            daughterVolumeZ += daughterThickness;
+
+            Geant4Basic daughterVolume = new G4Box("daughter_volume", chamberDX, chamberDY, daughterThickness);
+            daughterVolume.setName("region" + regionIndex + "_" + nameArray[i]);
+            daughterVolume.setMother(chamberVolume);
+            daughterVolume.setPosition(0.0, 0.0, daughterVolumeZ);
+
+            daughterVolumeZ += daughterThickness;
+        }
+    return  chamberVolume;
+ }
+
+    
+    
+    public double[] getChamberDimensions()
+    {
+        
+        double[] dim = new double[3];
+        
+
+        dim[2] = (this.getChamberThickness())/2. + LMUConstants.ZENLARGEMENT/2;
+        dim[1] = LMUConstants.YSIZE/2 + LMUConstants.YENLARGEMENT/2;
+        dim[0] = LMUConstants.XSIZE/2 + LMUConstants.XENLARGEMENT/2;
+        
+        return dim;
+        
+    }
+    /**
+     * Calculates the daughter chamber dimensions
+     * 
+     * @return an array of doubles containing trapezoid dimensions: half small base , half large base, half height
+     */
+    public double[] getDaughterChamberDimensions(){
+       
+        double[] dim = new double[3];
+        
+        dim[2] = (this.getChamberThickness())/2.;
+        dim[1] = LMUConstants.YSIZE/2 ;
+        dim[0] = LMUConstants.XSIZE/2 ;
+        
+        return dim;
+    }
+
+    /**
+     * Returns the chamber volume for the chosen sector and chamber
+     * 
+     * @param region (1-6)
+     * @return the chamber volume
+     */
+    public Geant4Basic getChamberVolume(int region) {
+
+        String volumeName = "region" + region + "_cathode_gas";
+        
+        return this.getAllVolumes().stream()
+                      .filter(volume -> (volume.getName().contains(volumeName)))
+                      .findAny()
+                      .orElse(null);
+    }
+
+    /**
+     * Returns the sector volume for the given sector number
+     * 
+     * @param region (1-6)
+     * @return the region volume
+     */
+    public Geant4Basic getRegionVolume(int region) {
+
+        String volName = "LMU_region" + region ;
+        return this.getAllVolumes().stream()
+                      .filter(volume -> (volume.getName().contains(volName)))
+                      .findAny()
+                      .orElse(null);
+    }
+
+    
+ 
+    
+    
+    public static void main(String[] args) {
+        DatabaseConstantProvider cp = new DatabaseConstantProvider(11, "default");
+
+        LMUConstants.connect(cp);
+        
+        LMUGeant4Factory factory = new LMUGeant4Factory(cp);
+            
+        factory.getAllVolumes().forEach(volume -> {
+            System.out.println(volume.gemcString());
+        });
+        
+     
+
+    }
+
+}
